ea53ec7fb07c192db20f859e46b18760e8df3e40
Pablo Hernandez <phernandezm07@gmail.com>
2026-05-16 01:19:18 -0500

    neovim: add description to keymaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -13,22 +13,34 @@ vim.o.complete = "."
 vim.o.confirm = true
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>f', function()
-  require("search"):search_file({
-    mise = {
-      global = false
-    }
-  })
-end)
+vim.keymap.set(
+  'n', '<leader>f',
+  function()
+    require("search"):search_file({
+      mise = {
+        global = false
+      }
+    })
+  end,
+  { desc = "Search files" }
+)
 
-vim.keymap.set('n', '<leader>s', function()
-  require("search"):rg({
-    mise = {
-      global = false
-    }
-  })
-end)
+vim.keymap.set(
+  'n', '<leader>s',
+  function()
+    require("search"):rg({
+      mise = {
+        global = false
+      }
+    })
+  end,
+  { desc = "Search pattern in files" }
+)
 
-vim.keymap.set('n', '<leader>r', function()
-  require("utils.qol"):reload_nvim_configuration()
-end)
+vim.keymap.set(
+  'n', '<leader>r',
+  function()
+    require("utils.qol"):reload_nvim_configuration()
+  end,
+  { desc = "Reload configuration" }
+)
