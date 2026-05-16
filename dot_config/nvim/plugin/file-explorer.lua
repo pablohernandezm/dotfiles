9b0f42dcbcddf159c13e44584ab83e4968e07ee1
Pablo Hernandez <phernandezm07@gmail.com>
2026-05-16 01:12:43 -0500

    neovim: add oil buffer maps
    Shift-L opens entry; Shift-H goes to parent path (buffer-local).

--- dot_config/nvim/plugin/file-explorer.lua
+++ dot_config/nvim/plugin/file-explorer.lua
@@ -6,15 +6,27 @@ vim.pack.add({
 local oil = require("oil")
 oil.setup({})
 
-vim.keymap.set("n", "<leader>e", function()
-  oil.toggle_split(
-    nil,
-    {
-      vertical = true,
-    },
-    function()
-      vim.api.nvim_win_set_width(0, 20)
-      vim.wo.number = false
-      vim.wo.relativenumber = false
-    end)
-end)
+vim.keymap.set(
+  "n", "<leader>e",
+  function()
+    oil.toggle_split(
+      nil,
+      {
+        vertical = true,
+      },
+      function()
+        vim.api.nvim_win_set_width(0, 20)
+        vim.wo.number = false
+        vim.wo.relativenumber = false
+      end)
+  end,
+  { desc = "Open file explorer" }
+)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "oil",
+  callback = function(ev)
+    vim.keymap.set("n", "<S-l>", "<CR>", { buf = ev.buf, remap = true, desc = "Open entry" })
+    vim.keymap.set("n", "<S-h>", "-", { buf = ev.buf, remap = true, desc = "Parent path" })
+  end
+})
