f9decf91f98b111be489c6432976a1501eee4c66
Pablo Hernandez <phernandezm07@gmail.com>
2026-06-06 01:37:08 -0500

    fix: disable pencil on markdown files

--- dot_config/nvim/lua/writing.lua
+++ dot_config/nvim/lua/writing.lua
@@ -5,7 +5,7 @@ local pencil_g = vim.api.nvim_create_augroup("pencil", { clear = true })
 local pencil_buffers = {}
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "typst", "markdown" },
+  pattern = { "typst" },
   group = pencil_g,
   callback = function(args)
     if not vim.list_contains(pencil_buffers, args.buf) then
