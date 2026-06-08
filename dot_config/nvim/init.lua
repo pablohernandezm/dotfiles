7d157eeb53f63f7cd5647d8977540bfa3bc108af
Pablo Hernandez <phernandezm07@gmail.com>
2026-06-08 17:56:14 -0500

    neovim: enable colorcolumn

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.diagnostic.config({
   virtual_text = true
 })
 
+vim.g.mapleader = " "
 vim.o.wrap = false
 vim.o.cindent = true
 vim.o.shiftwidth = 2
@@ -10,7 +11,7 @@ vim.o.expandtab = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.confirm = true
-vim.g.mapleader = " "
+vim.o.colorcolumn = "100"
 
 --- modules
 require("colorscheme")
