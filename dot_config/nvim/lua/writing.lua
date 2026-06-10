775a496e6ee3b94d495adaf613cfc2a1f9b2100b
Pablo Hernandez <phernandezm07@gmail.com>
2026-06-10 13:01:09 -0500

    fix: remove autostart of pencil

--- dot_config/nvim/lua/writing.lua
+++ dot_config/nvim/lua/writing.lua
@@ -1,16 +1,1 @@
 vim.pack.add({ "https://github.com/preservim/vim-pencil" })
-
-local pencil_g = vim.api.nvim_create_augroup("pencil", { clear = true })
-
-local pencil_buffers = {}
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "typst" },
-  group = pencil_g,
-  callback = function(args)
-    if not vim.list_contains(pencil_buffers, args.buf) then
-      vim.cmd("Pencil")
-      table.insert(pencil_buffers, args.buf)
-    end
-  end
-})
