9ae0b12572d8705913070453012a0959b7f6cc24
Pablo Hernandez <phernandezm07@gmail.com>
2026-06-28 19:24:23 -0500

    feat: use official copilot instead of copilot.lua

--- dot_config/nvim/lua/completion.lua
+++ dot_config/nvim/lua/completion.lua
@@ -4,6 +4,18 @@ vim.pack.add({
   "https://github.com/fang2hou/blink-copilot",
 })
 
+vim.g.copilot_no_maps = true
+
+-- Block the normal Copilot suggestions
+vim.api.nvim_create_augroup("github_copilot", { clear = true })
+vim.api.nvim_create_autocmd({ "FileType", "BufUnload" }, {
+  group = "github_copilot",
+  callback = function(args)
+    vim.fn["copilot#On" .. args.event]()
+  end,
+})
+vim.fn["copilot#OnFileType"]()
+
 --- Completion settings
 vim.pack.add({
   "https://github.com/saghen/blink.lib",
